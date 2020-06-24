31f79bfa3f5d590b6d1a4c911b7d96211b45458a
L33Chy03 <67312966+L33Chy03@users.noreply.github.com>
2020-06-24 15:01:31 +0200

    add del button

--- Pages/autre_chat.php
+++ Pages/autre_chat.php
@@ -14,6 +14,7 @@ $_SESSION['path'] = "Chats/Autre/";
 		<?php include "Menu/menu.php"; ?>
 		<div id="page">
 			<?php include "Add/btn_add.php"; ?>
+			<?php include "Del/btn_del.php"; ?>
 			<section>
 				<article>
 					<h3>Nos Autres Chats</h3>
